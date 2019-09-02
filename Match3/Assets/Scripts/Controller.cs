5e11bfa012b76c147e058fc878f855a6361235a8
fedepvg <federicopvg@gmail.com>
2019-09-02 12:58:00 -0300

    tiles drawn

--- Match3/Assets/Scripts/Controller.cs
+++ Match3/Assets/Scripts/Controller.cs
@@ -6,12 +6,15 @@ public class Controller : MonoBehaviour
 {
     Model GridModel;
     public View GridView;
-    
+    public int Width;
+    public int Height;
+
     void Start()
     {
         GridModel = new Model();
-        GridModel.CreateGrid(7, 10);
+        GridModel.CreateGrid(Width, Height);
         GridModel.SetGridPositions();
+        GridView.InitTiles(GridModel.GetGrid(), Width, Height);
     }
     
     void Update()
--- Match3/Assets/Scripts/View.cs
+++ Match3/Assets/Scripts/View.cs
@@ -9,7 +9,7 @@ public class View : MonoBehaviour
 
     void Start()
     {
-        
+
     }
 
     void Update()
@@ -17,13 +17,27 @@ public class View : MonoBehaviour
         
     }
 
-    void InitTiles(int[,] grid, int width, int height)
+    public void InitTiles(int[,] grid, int width, int height)
     {
+        Vector3 CameraMinBounds = CameraUtils.OrthographicBounds().min;
+        Vector3 CameraExents = CameraUtils.OrthographicBounds().extents;
+        float PrefabWidth = TilePrefab.GetComponent<SpriteRenderer>().sprite.bounds.size.x;
+        float PrefabHeight = TilePrefab.GetComponent<SpriteRenderer>().sprite.bounds.size.x;        
+        float GridWidth = CameraUtils.OrthographicBounds().size.x;
+        float GridHeight = CameraUtils.OrthographicBounds().size.y;
+        float bound = 0.16f;
+        int cant = 0;
+
         for (int i = 0; i < width; i++)
         {
             for (int j = 0; j < height; j++)
             {
-                GameObject go = Instantiate(TilePrefab);
+                GameObject go = Instantiate(TilePrefab,new Vector2(CameraMinBounds.x + bound + GridWidth / width * i + PrefabWidth / 2,
+                                            CameraMinBounds.y + bound + GridHeight / height * j + PrefabHeight / 2),Quaternion.identity);
+                go.name = "Tile" + cant;
+                SpriteRenderer TileRenderer = go.GetComponent<SpriteRenderer>();
+                TileRenderer.sprite = TileSprite[grid[i, j]];
+                cant++;
             }
         }
     }
