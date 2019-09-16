fe913a563fd447ba2ba29f04e16c0d4fa07f83a7
fedepvg <federicopvg@gmail.com>
2019-09-16 11:17:00 -0300

    raycast and keystore

--- Match3/Assets/Scripts/Controller.cs
+++ Match3/Assets/Scripts/Controller.cs
@@ -8,18 +8,30 @@ public class Controller : MonoBehaviour
     public View GridView;
     public int Width;
     public int Height;
+    GameObject[,] Blocks;
+    Vector2Int SelectedPosition;
+    bool IsBlockClicked;
 
     void Start()
     {
         GridModel = new Model();
         GridModel.CreateGrid(Width, Height);
         GridModel.SetGridPositions();
+        Blocks = new GameObject[Width, Height];
+
         for (int i = 0; i < Height; i++)
         {
             CheckHorizontalRepeated();
             CheckVerticalRepeated();
         }
-        GridView.InitTiles(GridModel.GetGrid(), Width, Height);
+
+        for (int i = 0; i < Width; i++)
+        {
+            for (int j = 0; j < Height; j++)
+            {
+                Blocks[i,j] = GridView.GetNewTile(i, j, Width, Height, GridModel.GetValue(i,j));
+            }
+        }
     }
 
     private void Update()
@@ -27,9 +39,11 @@ public class Controller : MonoBehaviour
         if (Input.GetMouseButtonDown(0))
         {
             RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
-            
-            if(hit)
-                Debug.Log("Target: " + hit.transform.name);
+
+            if (hit)
+            {
+                GetClickedBlock(hit.transform.gameObject);
+            }
             
         }
     }
@@ -86,6 +100,36 @@ public class Controller : MonoBehaviour
                 }
 
                 lastValue = actualValue;
+            }
+        }
+    }
+
+    public void GetClickedBlock(GameObject Block)
+    {
+        for (int i = 0; i < Width; i++)
+        {
+            for (int j = 0; j < Height; j++)
+            {
+                if (Block == Blocks[i, j])
+                {
+                    if(IsBlockClicked)
+                    {
+                        if(i==SelectedPosition.x - 1 || i == SelectedPosition.x + 1||
+                           j == SelectedPosition.y - 1 || j == SelectedPosition.y + 1)
+                        {
+                            IsBlockClicked = false;
+                            GameObject AuxGO = Block;
+                            Block = Blocks[SelectedPosition.x, SelectedPosition.y];
+                            Blocks[SelectedPosition.x, SelectedPosition.y] = AuxGO;
+                            Debug.Log("swichea");
+                        }
+                    }
+                    else
+                    {
+                        IsBlockClicked = true;
+                        SelectedPosition = new Vector2Int(i, j);
+                    }
+                }
             }
         }
     }
--- Match3/Assets/Scripts/View.cs
+++ Match3/Assets/Scripts/View.cs
@@ -6,34 +6,17 @@ public class View : MonoBehaviour
 {
     public GameObject TilePrefab;
     public Sprite[] TileSprite;
-    public GameObject[,] Blocks;
 
-    public void InitTiles(int[,] grid, int width, int height)
+    public GameObject GetNewTile(int GridXPos, int GridYPos, int GridWidth, int GridHeight, int BlockType)
     {
-        Vector3 CameraMinBounds = CameraUtils.OrthographicBounds().min;
-        Vector3 CameraExents = CameraUtils.OrthographicBounds().extents;
         float PrefabWidth = TilePrefab.GetComponent<SpriteRenderer>().sprite.bounds.size.x;
         float PrefabHeight = TilePrefab.GetComponent<SpriteRenderer>().sprite.bounds.size.x;        
-        float GridWidth = CameraUtils.OrthographicBounds().size.x;
-        float GridHeight = CameraUtils.OrthographicBounds().size.y;
-        int cant = 0;
+        GameObject Block = Instantiate(TilePrefab,new Vector2(PrefabWidth * GridXPos, PrefabHeight * GridYPos * -1), Quaternion.identity);
+        Block.name = "Tile";
+        Block.AddComponent<BoxCollider2D>();
+        SpriteRenderer TileRenderer = Block.GetComponent<SpriteRenderer>();
+        TileRenderer.sprite = TileSprite[BlockType];
 
-        Blocks = new GameObject[width, height];
-
-        for (int i = 0; i < width; i++)
-        {
-            for (int j = 0; j < height; j++)
-            {
-                //GameObject go = Instantiate(TilePrefab,new Vector2(CameraMinBounds.x + GridWidth / width * i + PrefabWidth,
-                //                            CameraMinBounds.y + GridHeight / height * j + PrefabHeight), Quaternion.identity);
-                Blocks[i,j]= Instantiate(TilePrefab, new Vector2(CameraMinBounds.x + GridWidth / width * i + PrefabWidth,
-                                            CameraMinBounds.y + GridHeight / height * j + PrefabHeight), Quaternion.identity);
-                Blocks[i, j].name = "Tile" + cant;
-                Blocks[i, j].AddComponent<BoxCollider2D>();
-                SpriteRenderer TileRenderer = Blocks[i, j].GetComponent<SpriteRenderer>();
-                TileRenderer.sprite = TileSprite[grid[i, j]];
-                cant++;
-            }
-        }
+        return Block;
     }
 }
